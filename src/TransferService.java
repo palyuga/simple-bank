f75225ce67a58ffc9c40e714d2c3f6b0f6e077c0
Dmitry Palyuga <dmpalyuga@devexperts.com>
2026-05-27 12:49:52 +0400

    Transaction commission support added

--- src/TransferService.java
+++ src/TransferService.java
@@ -1,8 +1,10 @@
 public class TransferService {
     public void transfer(Account from, Account to, double amount) {
-        if (from.getBalance() < amount)
+        double commission = Math.min(amount * 0.01, 10.00);
+        double totalToWithdraw = amount + commission;
+        if (from.getBalance() < totalToWithdraw)
             throw new RuntimeException("Not enough funds");
-        from.setBalance(from.getBalance() - amount);
+        from.setBalance(from.getBalance() - totalToWithdraw);
         to.setBalance(to.getBalance() + amount);
     }
 }
